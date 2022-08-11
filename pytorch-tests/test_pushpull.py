998b6aca93d26048a90a5fc3fec7fc6bc0d9f3dd
Sebastian Weiss <sebastian13.weiss@tum.de>
2022-08-11 14:25:10 +0200

    finished pushpull test

--- pytorch-tests/test_pushpull.py
+++ pytorch-tests/test_pushpull.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import torch
 import torch.nn.functional
 import torch.autograd
@@ -6,6 +7,8 @@ import torch.nn.init
 from typing import Optional
 import imageio
 import matplotlib.pyplot as plt
+import torch.optim
+import tqdm
 
 torch.classes.load_library(os.path.join(os.path.split(__file__)[0], "../bin/qmlp.so"))
 print(torch.classes.loaded_libraries)
@@ -14,13 +17,22 @@ torch.classes.qmlp.QuickMLP.set_debug_mode(True)
 pullpush = torch.classes.qmlp.utils.pullpush
 
 # load test image from the "Painter" dataset
-ground_truth_image = imageio.imread("340.jpg")
+ground_truth_image = imageio.imread("pushpull_input.jpg")
 print(ground_truth_image.shape)
 print(ground_truth_image.dtype)
 plt.figure()
 plt.imshow(ground_truth_image)
 plt.title("Input ground truth")
 plt.show()
+
+#prepare output
+os.makedirs("output", exist_ok=True)
+def save(img, name):
+    if len(img.shape)==3: #B,H,W
+        img = torch.stack([img,img,img], dim=1) # B,3,H,W
+    img = img[0] # first batch
+    img8 = np.clip(img.permute(1, 2, 0).cpu().numpy() * 255, 0, 255).astype(np.uint8)
+    imageio.imwrite(os.path.join("output", name), img8)
 
 # to PyTorch and to B,C,H,W format
 device = torch.device("cuda")
@@ -29,9 +41,12 @@ B, C, H, W = ground_truth_gpu.shape
 
 # create sparse mask with 5% of pixels set
 mask = (torch.rand((B, H, W), dtype=torch.float32, device=device) > 0.95) * 1.0
+mask2 =  mask.unsqueeze(1)
+save(mask, "pullpush-mask.png")
 
 # inpainting
 inpainted_image = pullpush(mask, ground_truth_gpu)
+save(inpainted_image, "pullpush-inpainted.png")
 # and show
 fig, axes = plt.subplots(1, 2)
 axes[0].imshow(mask[0].cpu().numpy())
@@ -39,3 +54,43 @@ axes[0].set_title("Mask")
 axes[1].imshow(inpainted_image[0].permute(1,2,0).cpu().numpy())
 axes[1].set_title("Inpainted")
 plt.show()
+
+# train input
+data_in = torch.rand_like(ground_truth_gpu)
+data_in.requires_grad_(True)
+adam = torch.optim.Adam([data_in], lr=0.2)
+for i in range(10):
+    adam.zero_grad()
+    out = pullpush(mask, data_in)
+    data_masked = (data_in * mask2).detach()
+    data_inpainted = out.detach()
+    loss = torch.nn.functional.mse_loss(out, ground_truth_gpu)
+    loss.backward()
+    adam.step()
+    save(data_masked, "pullpush-optim-DataMasked%02d.png"%i)
+    save(data_inpainted, "pullpush-optim-DataInpainted%02d.png" % i)
+    print("Optimize for the input data, loss:", loss.item())
+
+# train mask
+mask_raw_in = torch.randn_like(mask)
+mask_raw_in.requires_grad_(True)
+adam = torch.optim.Adam([mask_raw_in], lr=0.2)
+probability = 0.05 # 5% of pixels
+min_value = 1e-5
+for i in range(10):
+    adam.zero_grad()
+
+    # transform and normalize mask
+    importance1 = torch.nn.functional.softplus(mask_raw_in)
+    m = torch.mean(importance1,dim=[1, 2], keepdim=True)
+    m = torch.clamp(m, min=1e-7)
+    importance2 = min_value + importance1 * ((probability - min_value) / m)
+
+    out = pullpush(importance2, ground_truth_gpu)
+    data_inpainted = out.detach()
+    loss = torch.nn.functional.mse_loss(out, ground_truth_gpu)
+    loss.backward()
+    adam.step()
+    save(importance2.detach(), "pullpush-optim-MaskRaw%02d.png"%i)
+    save(data_inpainted, "pullpush-optim-MaskInpainted%02d.png" % i)
+    print("Optimize for the mask, loss:", loss.item())
