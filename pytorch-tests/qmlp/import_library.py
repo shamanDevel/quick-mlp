more network tests. Weight gradients are still wrong

pytorch-tests/qmlp/import_library.py
@@ -16,7 +16,7 @@ def load_library():
     print(torch.classes.loaded_libraries)
 
     torch.classes.qmlp.QuickMLP.set_compile_debug_mode(False)
-    torch.classes.qmlp.QuickMLP.set_verbose_logging(True)
+    torch.classes.qmlp.QuickMLP.set_verbose_logging(False)
     load_library.library_loaded = True
 
 load_library.library_loaded = False

pytorch-tests/qmlp/network.py
@@ -1,8 +1,10 @@
 import torch
 from torchtyping import TensorType
+import json
 
 from .import_library import load_library
 from .encoding import FusedEncoding
+from .activation import FusedActivation
 load_library()
 
 
@@ -27,13 +29,15 @@ class FusedNetwork(torch.nn.Module):
 
         self._network = torch.classes.qmlp.Network(cfg, parent)
         self._num_output_channels: int = self._network.channels_out()
+        self._num_layers: int = self._network.num_layers()
 
-        network_parameter = self._network.create_inference_parameters()
-        self._network.initialize_inference_parameters(network_parameter)
-        network_parameter.requires_grad_(True)
-        self.network_parameter = torch.nn.Parameter(network_parameter)
-        print("Number of trainable network parameters:", network_parameter.shape)
+        weights = self._network.create_inference_parameters()
+        self._network.initialize_inference_parameters(weights)
+        weights.requires_grad_(True)
+        self.weights = torch.nn.Parameter(weights)
+        print("Number of trainable network parameters:", weights.shape)
 
+        # encodings
         self._num_encodings: int = self._network.num_encodings()
         assert self._num_encodings > 0
         self._max_input_channel = 0
@@ -54,7 +58,7 @@ class FusedNetwork(torch.nn.Module):
                 dummy = torch.zeros((1,))
                 encoding_parameters[i] = torch.nn.Parameter(dummy)
         self.encoding_parameters = torch.nn.ParameterList(encoding_parameters)
-        print("Num input channels:", self.num_input_channels())
+        print("Num input channels:", self.channels_in())
 
     @staticmethod
     def MatrixSize() -> int:
@@ -108,19 +112,139 @@ class FusedNetwork(torch.nn.Module):
         """ The output channel count produced by the network """
         return self._network.channels_out()
 
-    # TODO: access to weights
+    def num_layers(self) -> int:
+        return self._num_layers
+
+    def get_weight(self, layer: int, bias: bool, grad: bool) -> torch.Tensor:
+        """
+        Returns a copy of the weight tensor
+        :param layer: the layer index
+        :param bias: True -> bias; False -> weight matrix
+        :param grad: True -> gradient; False -> inference parameters
+        :return: copy of the weight tensor
+        """
+        p = self.weights.grad if grad else self.weights.data
+        t = self._network.parameter_view(layer, bias, p)
+        return torch.clone(t.detach())
+
+    def set_weight(self, layer: int, bias: bool, grad: bool, value: torch.Tensor):
+        p = self.weights.grad if grad else self.weights.data
+        t = self._network.parameter_view(layer, bias, p)
+        assert value.shape == t.shape
+        assert value.dtype == t.dtype
+        t.copy_(value)
 
     def num_output_channels(self) -> int:
         return self._num_output_channels
 
-    def forward(self, input: torch.Tensor) -> torch.Tensor:
-        assert len(input.shape)==2
-        assert input.shape[1] == self.num_input_channels()
+    def forward(self, x: TensorType["batch", "in-channels", float]
+                ) -> TensorType["batch", "out-channels", float]:
+        assert len(x.shape)==2
+        assert x.shape[1] == self.channels_in()
 
-        return self._network.forward(input, self.network_parameter, []) #list(self.encoding_parameters))
+        return self._network.forward(x, self.weights, []) #list(self.encoding_parameters))
 
     def __repr__(self):
         return f"FusedNetwork(\"{self._cfg}\")"
 
     def __str__(self):
         return f"FusedNetwork(\"{self._cfg}\")"
+
+
+class SimulatedNetwork(torch.nn.Module):
+    """
+    Simulates the fused network in regular PyTorch.
+    """
+
+    def __init__(self, cfg: str):
+        """
+        Constructs a network module that simulates the fused network.
+        :param cfg:
+        """
+        super().__init__()
+        cfg = json.loads(cfg)
+        self._channels_in = cfg['num_inputs']
+        self._channels_out = cfg['num_outputs']
+
+        # verify encoding, only 'Identity' allowed in simulation mode
+        if len(cfg['encodings']) == 0:
+            # implicit identity encoding, all is fine.
+            pass
+        elif len(cfg['encodings']) == 1:
+            e = cfg['encodings'][0]
+            if e['id'] != "Identity" or e['start_in'] != 0 or e['n_in'] != self._channels_in:
+                raise ValueError("Only identity encoding supported and it must consume all inputs")
+        else:
+            raise ValueError("Only a single encoding supported")
+
+        # Create layers
+        layers = []
+        n_in = self._channels_in
+        n_out = None
+        self._has_bias = []
+        for layer in cfg['network']:
+            n_out = layer['n_out']
+            bias = layer['bias']
+            activation = FusedActivation.get_activation(layer['activation'])
+            layers.append(torch.nn.Linear(n_in, n_out, bias=bias, dtype=torch.float16))
+            layers.append(activation)
+            n_in = n_out
+            self._has_bias.append(bias)
+        if n_out != self._channels_out:
+            raise ValueError("Last layer does not match the number of outputs")
+        self.layers = torch.nn.Sequential(*layers)
+        self._num_layers = len(cfg['network'])
+
+    def channels_in(self):
+        return self._channels_in
+
+    def channels_out(self):
+        return self._channels_out
+
+    def num_layers(self) -> int:
+        return self._num_layers
+
+    def has_bias(self, layer: int):
+        return self._has_bias[layer]
+
+    def get_weight(self, layer: int, bias: bool, grad: bool) -> torch.Tensor:
+        """
+        Returns a copy of the weight tensor
+        :param layer: the layer index
+        :param bias: True -> bias; False -> weight matrix
+        :param grad: True -> gradient; False -> inference parameters
+        :return: copy of the weight tensor
+        """
+        layer = self.layers[2*layer]
+        assert isinstance(layer, torch.nn.Linear)
+        t = layer.bias if bias else layer.weight
+        return torch.clone(t.grad.detach()) if grad else torch.clone(t.detach())
+
+    def set_weight(self, layer: int, bias: bool, grad: bool, value: torch.Tensor):
+        layer = self.layers[2 * layer]
+        assert isinstance(layer, torch.nn.Linear)
+        t = layer.bias if bias else layer.weight
+        if grad:
+            t.grad = torch.clone(value.detach())
+        else:
+            t.copy_(value.detach())
+
+    def forward(self, x: TensorType["batch", "in-channels", float]
+                ) -> TensorType["batch", "out-channels", float]:
+        return self.layers(x.to(torch.float16)).to(torch.float32)
+
+
+def copy_torch_to_fused(dst: FusedNetwork, src: SimulatedNetwork, grad: bool = False):
+    """
+    Copies the weights (or gradients if grad=True) of src to dst
+    :param dst: the destination fused network
+    :param src: the source simulated network
+    :param grad: true if gradients should be copied instead of weights
+    """
+    assert dst.num_layers() == src.num_layers()
+    for layer in range(src.num_layers()):
+        w = src.get_weight(layer, False, grad)
+        dst.set_weight(layer, False, grad, w)
+        if src.has_bias(layer):
+            b = src.get_weight(layer, True, grad)
+            dst.set_weight(layer, True, grad, b)

pytorch-tests/qmlp_tests/test_network_base.py
@@ -1,5 +1,176 @@
-from import_library import load_library
+import itertools
+import os
+import numpy as np
+import torch
+import torch.nn.functional
+import torch.autograd
+import torch.nn.init
+import torch.optim
+import imageio
+from torchtyping import TensorType
+import json
+
+from qmlp.import_library import load_library
+from qmlp.network import FusedNetwork, SimulatedNetwork, copy_torch_to_fused
 load_library()
 
 
-if __name__ == '__main__':
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def _print_tensors(t1: np.ndarray, t2: np.ndarray, diff: np.ndarray,
+                   _prefix='', _suffix='\n'):
+    if len(t1.shape) == 2:
+        print(_prefix, "[", sep="", end="")
+        for i in range(t1.shape[0]):
+            _print_tensors(t1[i], t2[i], diff[i], '  ', ',\n')
+        print("]", end=_suffix)
+    elif len(t1.shape) == 1:
+        print(_prefix, "[", sep="", end="")
+        for i in range(t1.shape[0]):
+            v1 = t1[i]
+            v2 = t2[i]
+            d = diff[i]
+            if d:
+                print(f"{bcolors.WARNING} {v1:5.3f}|{v2:5.3f}{bcolors.ENDC}", end="")
+            else:
+                print(f" {v1:3f}|{v2:3f}", end="")
+        print(" ]", end=_suffix)
+    else:
+        raise ValueError("Can print only 1D and 2D tensors")
+
+
+def _compare_tensors(actual: torch.Tensor, expected: torch.Tensor, name: str,
+                     rtol=1e-5, atol=1e-8, fail_threshold=0.05):
+    actual = actual.detach().cpu().to(torch.float32).numpy()
+    expected = expected.detach().cpu().to(torch.float32).numpy()
+    same = np.isclose(actual, expected, rtol=rtol, atol=atol)
+    diff = ~same
+    if not np.all(same):
+        print(name)
+        _print_tensors(expected, actual, diff)
+        fraction_failed = np.mean(diff*1.0)
+        if fraction_failed > fail_threshold:
+            raise AssertionError(f"{name} Tensors are not the same and the number of errors {fraction_failed*100:.2f}% have exceeded the threshold")
+        else:
+            print(f"{bcolors.WARNING}{name} Tensors are not the same, but the number of errors {fraction_failed*100:.2f}% is within the tolerance{bcolors.ENDC}")
+
+
+def _compare_networks(actual: FusedNetwork, expected: SimulatedNetwork, grad:bool = False,
+                      rtol=1e-5, atol=1e-8):
+    for layer in range(expected.num_layers()):
+        w_expected = expected.get_weight(layer, False, grad)
+        w_actual = actual.get_weight(layer, False, grad)
+        _compare_tensors(w_actual, w_expected,
+                         f"Layer {layer} weights {'(grad)' if grad else ''}: ",
+                         rtol, atol)
+
+        if expected.has_bias(layer):
+            b_expected = expected.get_weight(layer, True, grad)
+            b_actual = actual.get_weight(layer, True, grad)
+            _compare_tensors(b_actual, b_expected,
+                             f"Bias {layer} weights {'(grad)' if grad else ''}: ",
+                             rtol, atol)
+
+
+def _test_network(cfg: str):
+    network_fused = FusedNetwork(cfg)
+    network_torch = SimulatedNetwork(cfg)
+    copy_torch_to_fused(network_fused, network_torch)
+    n_in = network_fused.channels_in()
+    n_out = network_fused.channels_out()
+
+    device = torch.device("cuda")
+    rtol = 1e-2
+    atol = 1e-3
+    rtol_wgrad = 1e-1
+
+    network_fused.to(device)
+    network_torch.to(device)
+
+    # number of elements to test
+    Nx = [16, 32]#, 32, 128, 577]
+    for N in Nx:
+        print(f"Run with N={N} input elements")
+        input = torch.randn((N, n_in), device=device, dtype=torch.float32)
+        grad_output = torch.randn((N, n_out), device=device, dtype=torch.float32)
+
+        # 1. Test if the output of the inference pass matches
+        with torch.no_grad():
+            output_expected = network_torch(input)
+            output_actual = network_fused(input)
+            _compare_tensors(output_actual, output_expected,
+                             f"Inference pass with {N} inputs",
+                             rtol, atol)
+
+        # backward
+        for p in network_fused.parameters():
+            p.grad = None
+        for p in network_fused.parameters():
+            p.grad = None
+
+        # 2. Test if the output of the forward pass matches
+        input_expected = torch.clone(input)
+        input_actual = torch.clone(input)
+        input_expected.requires_grad_(True)
+        input_actual.requires_grad_(True)
+        output_expected = network_torch(input_expected)
+        output_actual = network_fused(input_actual)
+        _compare_tensors(output_actual, output_expected,
+                         f"Forward pass with {N} inputs",
+                         rtol, atol)
+
+        # 3. backprop and compare gradients
+        torch.autograd.backward(output_expected, grad_output)
+        torch.autograd.backward(output_actual, grad_output)
+
+        # Compare input gradients
+        with torch.no_grad():
+            _compare_tensors(input_actual.grad, input_expected.grad,
+                             f"Backward pass with {N} inputs, inputs",
+                             rtol, atol)
+
+        # Compare parameter gradients
+        with torch.no_grad():
+            _compare_networks(network_fused, network_torch,
+                              True, rtol_wgrad, atol)
+
+
+def test_network_single_layer():
+
+    channels = [16, 32, 48]
+    activations = ["celu", "identity"]
+    for c_in, c_out, activation in itertools.product(channels, channels, activations):
+        cfg = f"""{{
+    "num_inputs": {c_in},
+    "num_outputs": {c_out},
+    "activation_specification": [
+        "qmlp/builtin-activations.json"
+      ],
+    "encodings": [
+        {{
+          "id": "Identity",
+          "start_in": 0,
+          "n_in": {c_in}
+        }}
+      ],
+    "network": [
+        {{
+          "n_out": {c_out},
+          "bias": false,
+          "activation": "{activation}"
+        }}
+      ]
+    }}"""
+        print(cfg)
+        _test_network(cfg)
+